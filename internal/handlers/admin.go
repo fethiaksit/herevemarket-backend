ded2ca35fcc30e9f5818ebc8c53678af5ed57bc0
Fethi Akşit <fethiaksit@Fethi-Dizustu-Bilgisayar.local>
2025-12-25 01:10:15 +0300

    admin sayfası düzenlendi

--- internal/handlers/admin.go
+++ internal/handlers/admin.go
@@ -3,17 +3,17 @@ package handlers
 import "github.com/gin-gonic/gin"
 
 func AdminLoginPage(c *gin.Context) {
-	c.HTML(200, "admin/login.html", gin.H{})
+	c.HTML(200, "login.html", gin.H{})
 }
 
 func AdminCategoriesPage(c *gin.Context) {
-	c.HTML(200, "admin/categories.html", gin.H{})
+	c.HTML(200, "categories.html", gin.H{})
 }
 
 func AdminProductsPage(c *gin.Context) {
-	c.HTML(200, "admin/products.html", gin.H{})
+	c.HTML(200, "products.html", gin.H{})
 }
 
 func AdminOrdersPage(c *gin.Context) {
-	c.HTML(200, "admin/orders.html", gin.H{})
+	c.HTML(200, "orders.html", gin.H{})
 }
